c4937881b963f25a099f9ec03e9bc98659dfa581
vaishnavi-chaudhari-27 <vaishnavichaudhari278@gmail.com>
2026-03-21 11:18:23 +0530

    Update loop.cpp with numbering

--- loops/loop.cpp
+++ loops/loop.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main(){
+    // que1
     int n = 0;
     bool isprime = true;
     for (int i=2; i<=n-1; i++){
@@ -17,6 +18,9 @@ int main(){
      }
      return 0;
     }
+
+
+    // que2
     int n= 40;
     int evensum =0;
      //print odd num //
@@ -29,11 +33,14 @@ int main(){
      return 0 ;
 
 
+    //  que3
     int n = 5;
     for(int i=1; i<=n; i++){
         cout << i;
     }
 
+
+    // que4
     int n = 50;
     int sum = 0;
     for(int i=1; i<=n; i++){
@@ -42,6 +49,8 @@ int main(){
         cout << "sum = " << sum <<endl;
         return 0;
     
+
+    // que5
     int n = 90;
     int count = 1;
     while(count <= n) {
